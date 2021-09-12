7e13c30ff89314a4fefb8f6bc095565a5367f26d
Erik Šabič <erik.sab@gmail.com>
2021-09-12 21:00:39 +0200

    fix use-module forms, formatting

--- .config/guix/include/base-system.scm
+++ .config/guix/include/base-system.scm
@@ -4,33 +4,32 @@
   #:use-module (srfi srfi-1)
   #:use-module (gnu)
   #:use-module (gnu system nss)
-  #:use-module (gnu services pm)
   #:use-module (gnu services cups)
   #:use-module (gnu services desktop)
   #:use-module (gnu services docker)
   #:use-module (gnu services networking)
+  #:use-module (gnu services nix)
+  #:use-module (gnu services pm)
   #:use-module (gnu services virtualization)
-  #:use-module (gnu packages wm)
+  #:use-module (gnu services xorg)
+  #:use-module (gnu packages audio)
+  #:use-module (gnu packages certs)
   #:use-module (gnu packages cups)
-  #:use-module (gnu packages vim)
-  #:use-module (gnu packages gtk)
-  #:use-module (gnu packages xorg)
   #:use-module (gnu packages emacs)
   #:use-module (gnu packages file-systems)
   #:use-module (gnu packages gnome)
-  #:use-module (gnu packages mtools)
+  #:use-module (gnu packages gtk)
   #:use-module (gnu packages linux)
-  #:use-module (gnu packages audio)
-  #:use-module (gnu packages ssh)
+  #:use-module (gnu packages mtools)
+  #:use-module (gnu packages package-management)
   #:use-module (gnu packages pulseaudio)
-  #:use-module (gnu packages web-browsers)
+  #:use-module (gnu packages shells)
+  #:use-module (gnu packages ssh)
   #:use-module (gnu packages version-control)
-  #:use-module (gnu packages package-management))
-
-(use-service-modules nix)
-(use-service-modules desktop xorg)
-(use-package-modules certs)
-(use-package-modules shells)
+  #:use-module (gnu packages vim)
+  #:use-module (gnu packages web-browsers)
+  #:use-module (gnu packages wm)
+  #:use-module (gnu packages xorg))
 
 ;; Allow members of the "video" group to change the screen brightness.
 (define %backlight-udev-rule
@@ -43,17 +42,21 @@
                   "RUN+=\"/run/current-system/profile/bin/chmod g+w /sys/class/backlight/%k/brightness\"")))
 
 (define %my-laptop-services
-  (modify-services %desktop-services
-                   (elogind-service-type config =>
-                                         (elogind-configuration (inherit config)
-                                                                (handle-lid-switch-external-power 'suspend)))
-                   (udev-service-type config =>
-                                      (udev-configuration (inherit config)
-                                                          (rules (cons %backlight-udev-rule
-                                                                       (udev-configuration-rules config)))))
-                   (network-manager-service-type config =>
-                                                 (network-manager-configuration (inherit config)
-                                                                                (vpn-plugins (list network-manager-openvpn))))))
+  (modify-services
+   %desktop-services
+   (elogind-service-type
+    config =>
+    (elogind-configuration (inherit config)
+                           (handle-lid-switch-external-power 'suspend)))
+   (udev-service-type
+    config =>
+    (udev-configuration (inherit config)
+                        (rules (cons %backlight-udev-rule
+                                     (udev-configuration-rules config)))))
+   (network-manager-service-type
+    config =>
+    (network-manager-configuration (inherit config)
+                                   (vpn-plugins (list network-manager-openvpn))))))
 
 (define %xorg-libinput-config
   "Section \"InputClass\"
