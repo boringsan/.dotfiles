94dd3050573f5609670d07a386ccfa4e10948861
Erik Šabič <erik.sab@gmail.com>
2021-09-24 16:17:48 +0200

    add spacing, fix indentation

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -326,44 +326,44 @@
 (defun efs/org-font-setup ()
   ;; Replace list hyphen with dot
   (font-lock-add-keywords 'org-mode
-			  '(("^ *\\([-]\\) "
-			     (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+                          '(("^ *\\([-]\\) "
+                             (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
 
   ;; Set faces for heading levels
   (dolist (face '((org-level-1 . 1.2)
-		  (org-level-2 . 1.1)
-		  (org-level-3 . 1.05)
-		  (org-level-4 . 1.0)
-		  (org-level-5 . 1.1)
-		  (org-level-6 . 1.1)
-		  (org-level-7 . 1.1)
-		  (org-level-8 . 1.1)))
+                  (org-level-2 . 1.1)
+                  (org-level-3 . 1.05)
+                  (org-level-4 . 1.0)
+                  (org-level-5 . 1.1)
+                  (org-level-6 . 1.1)
+                  (org-level-7 . 1.1)
+                  (org-level-8 . 1.1)))
     (set-face-attribute (car face) nil
-			:font "Cantarell"
-			:weight 'regular
-			:height (cdr face)))
+                        :font "Cantarell"
+                        :weight 'regular
+                        :height (cdr face)))
 
   ;; Make sure org-indent face is available
   (require 'org-indent)
 
   ;; Ensure that anything that should be fixed-pitch in Org files appears that way
   (set-face-attribute 'fixed-pitch nil
-		      :font "CaskaydiaCove NF-12")
+                      :font "CaskaydiaCove NF-12")
   (set-face-attribute 'org-block nil
-		      :foreground nil
-		      :inherit 'fixed-pitch)
+                      :foreground nil
+                      :inherit 'fixed-pitch)
   (set-face-attribute 'org-code nil
-		      :inherit '(shadow fixed-pitch))
+                      :inherit '(shadow fixed-pitch))
   (set-face-attribute 'org-table nil
-		      :inherit '(shadow fixed-pitch))
+                      :inherit '(shadow fixed-pitch))
   (set-face-attribute 'org-verbatim nil
-		      :inherit '(shadow fixed-pitch))
+                      :inherit '(shadow fixed-pitch))
   (set-face-attribute 'org-special-keyword nil
-		      :inherit '(font-lock-comment-face fixed-pitch))
+                      :inherit '(font-lock-comment-face fixed-pitch))
   (set-face-attribute 'org-meta-line nil
-		      :inherit '(font-lock-comment-face fixed-pitch))
+                      :inherit '(font-lock-comment-face fixed-pitch))
   (set-face-attribute 'org-checkbox nil
-		      :inherit 'fixed-pitch))
+                      :inherit 'fixed-pitch))
 
 (use-package org
   :hook (org-mode . efs/org-mode-setup)
@@ -396,7 +396,7 @@
 
 (defun efs/org-mode-visual-fill ()
   (setq visual-fill-column-width 100
-	visual-fill-column-center-text t)
+        visual-fill-column-center-text t)
   (visual-fill-column-mode 1))
 
 (use-package visual-fill-column
