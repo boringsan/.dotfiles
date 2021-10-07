96926ddb623d046a26cf9b579ae70c2f5a21ec5d
Erik Šabič <erik.sab@gmail.com>
2021-10-08 01:22:09 +0200

    Emacs: custom-set-variables: cumulative update

--- .emacs.d/custom-set-variables.el
+++ .emacs.d/custom-set-variables.el
@@ -9,8 +9,29 @@
  '(lsp-haskell-completion-snippets-on nil)
  '(lsp-haskell-formatting-provider "brittany")
  '(package-selected-packages
-   '(dashboard yasnippet yaml-mode lsp-ui flycheck company writeroom-mode which-key vertico solaire-mode org-roam org-bullets monkeytype modus-themes magit-popup magit lsp-haskell keyfreq ivy-rich hydra helpful general evil-surround evil-collection eshell-git-prompt elm-mode doom-themes doom-modeline counsel-projectile avy all-the-icons-dired))
- '(safe-local-variable-values '((eval add-hook 'after-save-hook #'org-babel-tangle 0 t))))
+   '(dired-rainbow dired-single dired-ranger dired-collapse dired-hacks-utils dired-hacks dired-launch lsp-treemacs lsp-ivy dashboard yasnippet yaml-mode lsp-ui flycheck company writeroom-mode which-key vertico solaire-mode org-roam org-bullets monkeytype modus-themes magit-popup magit lsp-haskell keyfreq ivy-rich hydra helpful general evil-surround evil-collection eshell-git-prompt elm-mode doom-themes doom-modeline counsel-projectile avy all-the-icons-dired))
+ '(safe-local-variable-values
+   '((eval modify-syntax-entry 43 "'")
+     (eval modify-syntax-entry 36 "'")
+     (eval modify-syntax-entry 126 "'")
+     (eval let
+	   ((root-dir-unexpanded
+	     (locate-dominating-file default-directory ".dir-locals.el")))
+	   (when root-dir-unexpanded
+	     (let*
+		 ((root-dir
+		   (expand-file-name root-dir-unexpanded))
+		  (root-dir*
+		   (directory-file-name root-dir)))
+	       (unless
+		   (boundp 'geiser-guile-load-path)
+		 (defvar geiser-guile-load-path 'nil))
+	       (make-local-variable 'geiser-guile-load-path)
+	       (require 'cl-lib)
+	       (cl-pushnew root-dir* geiser-guile-load-path :test #'string-equal))))
+     (eval setq-local guix-directory
+	   (locate-dominating-file default-directory ".dir-locals.el"))
+     (eval add-hook 'after-save-hook #'org-babel-tangle 0 t))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
