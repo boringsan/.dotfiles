fd96c636ddd3e067b280dbe717364489e5750711
Erik Šabič <erik.sab@gmail.com>
2021-10-27 19:53:57 +0200

    .emacs.d/custom-set-variables.el: reduce the number of packages

--- .emacs.d/custom-set-variables.el
+++ .emacs.d/custom-set-variables.el
@@ -1,15 +1,13 @@
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("a9a67b318b7417adbedaab02f05fa679973e9718d9d26075c6235b1f0db703c8" "0b3aee906629ac7c3bd994914bf252cf92f7a8b0baa6d94cb4dfacbd4068391d" "f91395598d4cb3e2ae6a2db8527ceb83fed79dbaf007f435de3e91e5bda485fb" default))
- '(lsp-haskell-completion-snippets-on nil)
  '(lsp-haskell-formatting-provider "brittany")
+ '(custom-safe-themes
+   '("990e24b406787568c592db2b853aa65ecc2dcd08146c0d22293259d400174e37" "a9a67b318b7417adbedaab02f05fa679973e9718d9d26075c6235b1f0db703c8" default))
  '(package-selected-packages
-   '(mini-frame mixed-pitch org-appear ess bufler popper dired-rainbow dired-single dired-ranger dired-collapse dired-hacks-utils dired-hacks dired-launch lsp-treemacs lsp-ivy dashboard yasnippet yaml-mode lsp-ui flycheck company writeroom-mode which-key vertico solaire-mode org-roam org-bullets monkeytype modus-themes magit-popup magit lsp-haskell keyfreq ivy-rich hydra helpful general evil-surround evil-collection eshell-git-prompt elm-mode doom-themes doom-modeline counsel-projectile avy all-the-icons-dired))
+   '(dired-collapse dired-ranger dired-single dired-rainbow org-roam org-appear visual-fill-column org-bullets elm-mode lsp-haskell lsp-treemacs lsp-ivy lsp-ui company lsp-mode projectile magit evil-surround dashboard))
  '(safe-local-variable-values
    '((eval modify-syntax-entry 43 "'")
      (eval modify-syntax-entry 36 "'")
@@ -38,4 +36,3 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-(put 'upcase-region 'disabled nil)
