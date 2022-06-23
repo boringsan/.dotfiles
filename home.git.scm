4617d5ded1c323e85e8d35804c375f5ba6a45aa0
Erik Šabič <erik.sab@gmail.com>
2022-06-23 16:40:27 +0200

    new file: home.git.scm

---
+++ home.git.scm
@@ -0,0 +1,25 @@
+(use-modules
+ (gnu home services)
+ (gnu home)
+ (gnu packages version-control)
+ (gnu packages)
+ (gnu services)
+ (guix gexp))
+
+(home-environment
+ (packages
+  (map specification->package
+       (list "git"
+             "gitolite")))
+ (services
+  (list
+   (service home-bash-service-type
+            (home-bash-configuration
+             (bashrc
+              (list
+               ;; this is for sshd to eval before running gitolite-shell
+               (plain-file "init-bashrc" "PATH=$HOME/.guix-home/profile/bin")))))
+   (simple-service
+    'gitolite-rebuild-hooks
+    home-activation-service-type
+    #~(system* #$(file-append gitolite "/bin/gitolite") "setup" "--hooks-only")))))
