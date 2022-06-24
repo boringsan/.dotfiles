b9e4c22cf45e8d8456430ea0cb79bbdc80631694
Erik Šabič <erik.sab@gmail.com>
2022-06-24 07:28:31 +0200

    general.scm: reorganize and add packages from default

--- .config/guix/manifests/general.scm
+++ .config/guix/manifests/general.scm
@@ -1,40 +1,68 @@
 
+
+(define %general-packages
+  '("cowsay"
+    "fortune-mod"
+    "git"
+    "git:send-email"
+    "gnupg"
+    "graphviz"
+    "html-xml-utils"
+    "htop"
+    "lm-sensors"
+    "make"
+    "myrepos"
+    "openssh"
+    "pandoc"
+    "pkg-config"
+    "sicp"
+    "smartmontools"
+    "stow"
+    "universal-ctags"
+    "unzip"
+    "watchexec"
+    "xdg-utils"
+    "xdg-utils"
+    "zstd"))
+
+(define %c-packages
+  '("clang"
+    "cmake"
+    "gcc-toolchain"))
+
+(define %tex-packages
+  '("texlive"
+    "texlive-cm"
+    "texlive-fonts-ec"
+    "texlive-fonts-latex"
+    "texlive-fourier"
+    "texlive-latex-base"
+    "texlive-mathdesign"
+    "texlive-utopia"))
+
+(define %language-packages
+  '("dune"
+    "elm"
+    "ghostscript"
+    "guile"
+    "mercury"
+    "mono"
+    "node"
+    "ocaml"
+    "ocaml-utop"
+    "opam"
+    "python"
+    "r"
+    "r-igraph"
+    "r-rgl"
+    "ruby-nokogiri"
+    "ruby@2.7"
+    "rust"
+    ;; "rust-cargo"
+    "swi-prolog"))
+
 (specifications->manifest
-'("clang"
-   "cmake"
-   "cowsay"
-   "fortune-mod"
-   "gcc-toolchain"
-   "ghostscript"
-   "graphviz"
-   "guile"
-   "lm-sensors"
-   "make"
-   "mercury"
-   "myrepos"
-   "nushell"
-   "openssh"
-   "pandoc"
-   "pkg-config"
-   "python"
-   "r"
-   "r-igraph"
-   "r-rgl"
-   "rust"
-   "rust-cargo"
-   "sicp"
-   "smartmontools"
-   "stow"
-   "swi-prolog"
-   "texlive"
-   "texlive-cm"
-   "texlive-fonts-ec"
-   "texlive-fonts-latex"
-   "texlive-fourier"
-   "texlive-latex-base"
-   "texlive-mathdesign"
-   "texlive-utopia"
-   "unzip"
-   "watchexec"
-   "xdg-utils"
-   "zstd"))
+ (append %c-packages
+         %general-packages
+         %language-packages
+         %tex-packages))
